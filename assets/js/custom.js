1fa01660f32e693e09122efb42a0e924cb39fbe2
PSGOgli7 <nagy.leved.samuel@diak.szbi-pg.hu>
2022-12-07 09:49:42 +0100

    4ora

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -74,7 +74,7 @@
 
 	(function init() {
 	  function getTimeRemaining(endtime) {
-	    var t = Date.parse(endtime) - Date.parse(new Date());
+	    var t = Date.parse(endtime) - Date.parse(new Date("June 16, 2022 23:59:59").getTime);
 	    var seconds = Math.floor((t / 1000) % 60);
 	    var minutes = Math.floor((t / 1000 / 60) % 60);
 	    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
